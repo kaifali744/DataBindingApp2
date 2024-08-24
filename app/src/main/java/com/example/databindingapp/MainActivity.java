ff27db9da8733046620ddffbbb2bb1cfa6c666f5
Mohammad Kaif Ali <kaifali744@gmail.com>
2024-08-24 14:32:21 +0530

    all files upload

---
+++ app/src/main/java/com/example/databindingapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.databindingapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+
+import com.example.databindingapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding activityMainBinding;
+    private MyClickHandler clickHandler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Person p1 = new Person("Mohammad Kaif Ali","Jack@gmail.com");
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        activityMainBinding.setPerson(p1);
+
+        clickHandler = new MyClickHandler(this);
+        activityMainBinding.setClickHandler(clickHandler);
+    }
+}
---
+++ app/src/main/java/com/example/databindingapp/MyClickHandler.java
@@ -0,0 +1,18 @@
+package com.example.databindingapp;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.Toast;
+
+public class MyClickHandler {
+    private Context context;
+
+    public MyClickHandler(Context context) {
+        this.context = context;
+    }
+    public void onButtonClicked(View view){
+        Toast.makeText(context,
+                "Button is Clicked!",
+                Toast.LENGTH_SHORT).show();
+    }
+}
